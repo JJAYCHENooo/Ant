8063cd7d0199883e34f5ae9e07ac28b6e0cac3ee
mufanx <35735090+mufanx@users.noreply.github.com>
2019-09-23 19:02:00 +0800

    部分UI，找buging

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -6,6 +6,95 @@ import java.awt.event.ActionListener;
 public class UserPanel implements PlayRoomUpdateViewService {
     private UserPanelSendParamService userPanelSendParamService;
 
+    //UI组件
+    private JButton buttonStart = new JButton("Start");
+    private JButton buttonReset = new JButton("Reset");
+    private JButton buttonAutoPlay = new JButton("AutoPlay");
+    private JLabel labelMaxTime = new JLabel("maxTime:");
+    private JLabel labelMinTime = new JLabel("minTime:");
+    private JLabel labelTimeCount = new JLabel("0");
+    private int width = 900;
+    private int height = 500;
+    private int scale = width / 300;
+
+    //蚂蚁
+    private JLabel[] ants = new JLabel[5];
+    private int[] antsDirections = new int[]{-1, -1, -1, -1, -1};
+    private JLabel[] antsIdLabel = new JLabel[5];
+    private int[] antsPosition = new int[]{30, 80, 110, 160, 250};
+    private JButton antsDirectionsChange[] = new JButton[5];
+    private int antY = 200;
+    private int labelY = 150;
+    private int buttonY = 280;
+
+    //UI布局
+    private JPanel mainPanel = new JPanel();
+    private JPanel panelMenu = new JPanel();
+    private JPanel panelRecord = new JPanel();
+    private JPanel panelCommand = new JPanel();
+    private JFrame frame;
+
+    public UserPanel() {
+        frame = new JFrame("Ant Game");
+
+        frame.setBounds(400, 400, width, height);
+        frame.setIconImage(new ImageIcon("./Ant/resources/ant.png").getImage());
+
+        //布局
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.add(panelMenu, BorderLayout.NORTH);
+        panelMenu.add(panelRecord, BorderLayout.WEST);
+        panelMenu.add(panelCommand, BorderLayout.EAST);
+        panelCommand.add(buttonReset);
+        panelCommand.add(buttonStart);
+        panelCommand.add(buttonAutoPlay);
+        panelRecord.add(labelMaxTime, BorderLayout.NORTH);
+        panelRecord.add(labelMinTime, BorderLayout.SOUTH);
+        panelMenu.add(labelTimeCount, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //ant
+        for (int i = 0; i < 5; i++) {
+            ants[i] = new JLabel(new ImageIcon("./Ant/resources/antSLeft.png"));
+            antsIdLabel[i] = new JLabel(String.valueOf(i + 1));
+            antsDirectionsChange[i] = new JButton("Turn");
+        }
+
+        mainPanel.setLayout(null);
+        setAntsAtStart();
+
+        //事件响应
+        buttonStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userPanelSendParamService.createGame(antsDirections);
+            }
+        });
+        for(int i = 0; i < 5; i++) {
+            final int finalI = i;
+            antsDirectionsChange[i].addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        setAntDirection(finalI, antsDirections[finalI] * -1);
+                    }
+                });
+        }
+    }
+
+    private void setAntsAtStart() {
+        for(int i = 0; i < 5; i++) {
+            mainPanel.add(ants[i]);
+            ants[i].setBounds(antsPosition[i] * scale, antY, 50, 30);
+            mainPanel.add(antsIdLabel[i]);
+            antsIdLabel[i].setBounds(antsPosition[i] * scale, labelY, 30, 30);
+            mainPanel.add(antsDirectionsChange[i]);
+            antsDirectionsChange[i].setBounds(antsPosition[i] * scale, buttonY, 60, 30);
+
+        }
+    }
+
     public void setUserPanelSendParamService(UserPanelSendParamService userPanelSendParamService) {
         this.userPanelSendParamService = userPanelSendParamService;
     }
@@ -28,17 +117,33 @@ public class UserPanel implements PlayRoomUpdateViewService {
 
     @Override
     public void updateView(int[] antsPositions, int[] antsDirections, int timeCount) {
+        labelTimeCount.setText(String.valueOf(timeCount));
+        for(int i = 0; i < 5; i++) {
+            ants[i].setLocation(antsPositions[i], antY);
+            antsIdLabel[i].setLocation(antsPosition[i], labelY);
+            setAntDirection(i, antsDirections[i]);
+        }
+    }
 
+    private void setAntDirection(int antIndex, int antDirection) {
+        if(antDirection == antsDirections[antIndex]) return;
+        if(antDirection == 1) {
+            ants[antIndex].setIcon(new ImageIcon("./Ant/resources/antSRight.png"));
+        }
+        else ants[antIndex].setIcon(new ImageIcon("./Ant/resources/antSLeft.png"));
+        antsDirections[antIndex] = antDirection;
     }
 
     @Override
     public void updateRecord(int maxTime, int minTime) {
+        labelMaxTime.setText("maxTime: " + maxTime);
+        labelMinTime.setText("minTime: " + minTime);
 
     }
 
     @Override
     public void resetView() {
-
+        setAntsAtStart();
     }
 
 //    private class AntIDLabel {
