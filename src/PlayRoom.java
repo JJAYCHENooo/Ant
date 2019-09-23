1261f8671ca0356a41c10a8760d3a3dfe9f55e30
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-09-23 19:03:33 +0800

    部分注释

--- src/PlayRoom.java
+++ src/PlayRoom.java
@@ -3,41 +3,59 @@ public class PlayRoom implements CreepingGameUpdateViewService, UserPanelSendPar
 
     private PlayRoomUpdateViewService playRoomUpdateViewService;
 
-    private int lengthOfPole = 300;
-    private int velocityOfAnts = 5;
-    private int incTime = 1;
-    private int[] antsPositions = {30, 80, 110, 160, 250};
+    /** 游戏的一些静态常量 */
+    private final int lengthOfPole = 300;
+    private final int velocityOfAnts = 5;
+    private final int incTime = 1;
+    private final int[] antsPositions = {30, 80, 110, 160, 250};
 
+    /**
+     * maxTime: 历史记录中的最大时间
+     * minTime: 历史记录中的最小时间
+     * 配合 isPlayed 来更新数据
+     */
     private int maxTime = 0;
     private int minTime = 0;
+
+    /**
+     * isPlayed: 标识用户是否完成过一次游戏，帮助更新 maxTime / minTime
+     */
     private boolean isPlayed = false;
 
+    /**
+     * autoIndex: Autoplay 模式中的次数索引
+     */
     private int autoIndex = 0;
+    /**
+     * isAutoplay: 用于标记现在是否处于 Autoplay 模式
+     */
     private boolean isAutoplay = false;
 
-//    private int[] antsDirections = {-1, -1, -1, -1, -1};
-
-//    public void setAntsDirections(int[] antsDirections) {
-//        this.antsDirections = antsDirections;
-//    }
-
+    /**
+     * 设置 PlayRoom 的 playRoomUpdateViewService 变量
+     * @param playRoomUpdateViewService 传递实现了 PlayRoomUpdateViewService 接口的 UserPanel.
+     */
     public void setPlayRoomUpdateViewService(PlayRoomUpdateViewService playRoomUpdateViewService) {
         this.playRoomUpdateViewService = playRoomUpdateViewService;
     }
 
-    public static void main(String[] args) {
-        UserPanel userPanel = new UserPanel();
-        PlayRoom playRoom = new PlayRoom();
-
-        playRoom.setPlayRoomUpdateViewService(userPanel);
-        userPanel.setUserPanelSendParamService(playRoom);
-    }
-
+    /**
+     * 当 creepingGame 更新下一步时，便会调用该函数。该函数调用 playRoomUpdateViewService.updateView
+     * 让 UserPanel 更新蚂蚁的 UI.
+     * @param antsPositions
+     * @param antsDirections
+     * @param timeCount
+     */
     @Override
     public void updateView(int[] antsPositions, int[] antsDirections, int timeCount) {
         playRoomUpdateViewService.updateView(antsPositions, antsDirections, timeCount);
     }
 
+    /**
+     * 当 creepingGame 结束时（所有蚂蚁离开木杆），便会调用该函数。该函数传递更新 maxTime
+     * 和 minTime 给 UserPanel. 通过调用 playRoomUpdateViewService.updateRecord 实现.
+     * @param time
+     */
     @Override
     public void updateRecordTime(int time) {
         if (!isPlayed) {
@@ -51,6 +69,13 @@ public class PlayRoom implements CreepingGameUpdateViewService, UserPanelSendPar
         playRoomUpdateViewService.updateRecord(maxTime, minTime);
     }
 
+    /**
+     * 当creepingGame 结束时（所有蚂蚁离开木杆），便会调用该函数。
+     * 1. 负责摧毁游戏
+     * 2. 如果是 Autoplay 模式，会自动开启下一次游戏。
+     *  2.1 autoRunGame()
+     *  2.2 autoIndex++
+     */
     @Override
     public void gameEndedMessage(){
         resetGame();
@@ -59,7 +84,6 @@ public class PlayRoom implements CreepingGameUpdateViewService, UserPanelSendPar
             autoIndex++;
         }
     }
-
 
     @Override
     public void createGame(int[] antsDirections) {
@@ -86,4 +110,20 @@ public class PlayRoom implements CreepingGameUpdateViewService, UserPanelSendPar
             isAutoplay = false;
         }
     }
+
+    /**
+     * 整个程序的入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        UserPanel userPanel = new UserPanel();
+        PlayRoom playRoom = new PlayRoom();
+
+        /**
+         * 传递接口
+         */
+        playRoom.setPlayRoomUpdateViewService(userPanel);
+        userPanel.setUserPanelSendParamService(playRoom);
+    }
+
 }
